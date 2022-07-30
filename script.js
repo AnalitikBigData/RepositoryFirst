c81d4cccad3029d6be25a923bddffa2904110689
Veronika <morozznika@gmail.com>
2022-07-30 18:32:37 +0300

    инициализация переменных и добавление вывода в консоль

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = 'project', 
+screens = 'Простые, Сложные, Интерактивные', 
+screenPrice = 36739, 
+rollback = 10, 
+fullPrice = 5699108621, 
+adaptive = true;
+console.log(typeof(title));
+console.log(typeof(fullPrice));
+console.log(typeof(adaptive));
+console.log(screens.length);
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани');
+console.log('Стоимость разработки сайта '+ fullPrice + ' рублей/долларов/гривен/юани');
+console.log((screens.toLowerCase()).split(', '));
+console.log('процент отката посреднику за работу '+ (fullPrice*(rollback/100)));
 alert("Hello");
 console.log('console works');
