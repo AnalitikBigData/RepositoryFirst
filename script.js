949b57e21979a3efe50922fca3841cba3be577f4
Veronika <morozznika@gmail.com>
2022-08-01 14:06:28 +0300

    исправления для лучшего просмотра

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let title = 'project', 
-screens = 'Простые, Сложные, Интерактивные', 
-screenPrice = 36739, 
-rollback = 10, 
-fullPrice = 5699108621, 
-adaptive = true;
+    screens = 'Простые, Сложные, Интерактивные', 
+    screenPrice = 36739, 
+    rollback = 10, 
+    fullPrice = 5699108621, 
+    adaptive = true;
+
+
 console.log(typeof(title));
 console.log(typeof(fullPrice));
 console.log(typeof(adaptive));
@@ -11,6 +13,9 @@ console.log(screens.length);
 console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани');
 console.log('Стоимость разработки сайта '+ fullPrice + ' рублей/долларов/гривен/юани');
 console.log((screens.toLowerCase()).split(', '));
-console.log('процент отката посреднику за работу '+ (fullPrice*(rollback/100)));
+console.log('процент отката посреднику за работу '+ ( fullPrice * (rollback/100) ));
+
+
 alert("Hello");
+
 console.log('console works');
