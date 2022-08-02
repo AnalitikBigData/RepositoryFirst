048b106e1096627daa3501de76be4583e3972aec
Veronika <morozznika@gmail.com>
2022-08-02 16:02:04 +0300

    изменен adaptive и добавлен вывод в консоль

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-// 2 lesson 
+
 let title = 'project', 
     screens = 'Простые, Сложные, Интерактивные', 
     screenPrice = 36739, 
@@ -8,41 +8,41 @@ let title = 'project',
     adaptive = true;
 
 
-console.log(typeof(title));
+/*console.log(typeof(title));
 console.log(typeof(fullPrice));
 console.log(typeof(adaptive));
 console.log(screens.length);
 console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани');
 console.log('Стоимость разработки сайта '+ fullPrice + ' рублей/долларов/гривен/юани');
-console.log((screens.toLowerCase()).split(', '));
-console.log('процент отката посреднику за работу '+ ( fullPrice * (rollback/100) ));
+console.log( screens.toLowerCase().split(', '));
+console.log('процент отката посреднику за работу '+ ( fullPrice * (rollback/100) ));*/
 
-
-alert("Hello");
-
-console.log('console works');
-
-// 3 lesson
 
 title = prompt('Как называется ваш проект?');
-//console.log(title);
 screens = prompt('Какие типы экранов нужно разработать?');
-//console.log(screens);
 screenPrice = +prompt('Сколько будет стоить данная работа?');
-//console.log(screenPrice);
-adaptive = Boolean(prompt('Нужен ли адаптив на сайте?'));
-//console.log(adaptive);
+adaptive = confirm('Нужен ли адаптив на сайте?');
 
-let setvice1 = prompt('Какой дополнительный тип услуги нужен?');
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
-let setvice2 = prompt('Какой дополнительный тип услуги нужен?');
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 
 fullPrice = screenPrice + servicePrice1 + servicePrice2;
-console.log(fullPrice);
 
 let servicePercentPrice = Math.ceil( fullPrice - fullPrice * (rollback/100) );
-console.log(servicePercentPrice);
+
+
+console.log('Название проекта: ' + title);
+console.log('Типы экранов: ' + screens);
+console.log('Стоимость данной работы = ' + screenPrice);
+console.log('Адаптив - ' + adaptive);
+console.log('Тип дополнительной услуги: ' + service1);
+console.log('Стоимость дополнительной услуги = ' + servicePrice1);
+console.log('Тип дополнительной услуги: ' + service2);
+console.log('Стоимость дополнительной услуги = ' + servicePrice2);
+console.log('Стоимость разработки сайта '+ fullPrice + ' рублей/долларов/гривен/юани');
+console.log('Процент разработчику = ' + servicePercentPrice);
 
 
 // проверка на скидку
